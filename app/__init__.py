aa64b3041de23344e5bbd4c0f2c54e90cb6e76dc
Magic-cat-No765 <ndbghdvn@gmail.com>
2022-05-11 10:08:25 +0700

    fix CORS

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+origins = [
+    r"https://mangamew.vercel.app/",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # from . import urls
 from . import mangadex_api
