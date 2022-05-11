537f8032f527f1c79c024b097e1265fbd779342d
Magic-cat-No765 <ndbghdvn@gmail.com>
2022-05-11 11:27:05 +0700

    add post method

--- app/mangadex_api.py
+++ app/mangadex_api.py
@@ -1,5 +1,8 @@
+import json
+
 import requests
-from fastapi import Request
+from fastapi import Request, Response, status
+from fastapi.responses import HTMLResponse
 from app import app
 
 
@@ -8,13 +11,29 @@ def get_all_query_params(req: Request) -> dict:
 
 
 @app.get('/{req_path:path}')
-async def test_path(req_path: str, req: Request):
+async def test_path(req_path: str, req: Request, response: Response):
     try:
         url = fr"https://api.mangadex.org/{req_path}?{str(req.query_params)}"
         result = requests.get(url)
         if 'json' in result.headers['content-type']:
+            response.status_code = result.status_code
             return result.json()
         else:
             return {'error': 'You are trying to read non-json content-type which we are currently not supporting'}
+    except Exception as e:
+        return {'error': str(e)}
+
+
+@app.post('/{req_path:path}')
+async def test_path(req_path: str, req: Request, response: Response):
+    try:
+        url = fr"https://api.mangadex.org/{req_path}"
+        data = json.dumps(await req.json())
+        result = requests.post(url, data=data)
+        if 'json' in result.headers['content-type']:
+            response.status_code = result.status_code
+            return result.json()
+        else:
+            return HTMLResponse(result.content)
     except Exception as e:
         return {'error': str(e)}
