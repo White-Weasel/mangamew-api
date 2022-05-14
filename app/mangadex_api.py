9c0a7bb753ce501bd4d832b52ead5dc2958ffbd2
Magic-cat-No765 <ndbghdvn@gmail.com>
2022-05-14 08:34:38 +0700

    trying to add header

--- app/mangadex_api.py
+++ app/mangadex_api.py
@@ -11,15 +11,17 @@ def get_all_query_params(req: Request) -> dict:
 
 
 @app.get('/{req_path:path}')
-async def test_path(req_path: str, req: Request, response: Response):
+async def get_proxy(req_path: str, req: Request, response: Response):
     try:
         url = fr"https://api.mangadex.org/{req_path}?{str(req.query_params)}"
         result = requests.get(url, headers=dict(req.headers))
+        print(dict(req.headers))
         if 'json' in result.headers['content-type']:
             response.status_code = result.status_code
             return result.json()
         else:
-            return {'error': 'You are trying to read non-json content-type which we are currently not supporting'}
+            # return {'error': 'You are trying to read non-json content-type which we are currently not supporting'}
+            return HTMLResponse(result.content)
     except Exception as e:
         return {'error': str(e)}
 
